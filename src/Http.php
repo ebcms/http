1a9414fe87059e1a8ec06bbd24a74206e03d750e
ebcms <56634967+ebcms@users.noreply.github.com>
2022-03-03 10:00:22 +0800

    Create Http.php

---
+++ src/Http.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ebcms\Http;
+
+class Http
+{
+
+    public static function get(string $url, $timeout = 5, $ssl_verify = false)
+    {
+        $options = [
+            'http' => [
+                'method' => 'GET',
+                'timeout' => $timeout,
+            ],
+        ];
+        if ($ssl_verify === false) {
+            $options['ssl'] = [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ];
+        }
+        $response = file_get_contents($url, false, stream_context_create($options));
+        if (false === $response) {
+            throw new HttpException('post(' . $url . ') failure!');
+        }
+        return $response;
+    }
+
+    public static function post(string $url, array $data = [], $timeout = 5, $ssl = false)
+    {
+        $content = http_build_query($data);
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'timeout' => $timeout,
+                'header' => "Content-Type: application/x-www-form-urlencoded\r\nContent-Length: " . mb_strlen($content),
+                'content' => $content,
+            ],
+        ];
+        if ($ssl === false) {
+            $options['ssl'] = [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ];
+        }
+        $response = file_get_contents($url, false, stream_context_create($options));
+        if (false === $response) {
+            throw new HttpException('post(' . $url . ') failure!');
+        }
+        return $response;
+    }
+}
